084c8eba331bb1d0e130f5f062033b069a9f9cb0
leverh <leverh@gmail.com>
2023-08-21 09:14:24 +0000

    Modified the addbook index.js to include all the fields i planned on having from the backend

--- book-review-frontend/src/components/AddBook/index.js
+++ book-review-frontend/src/components/AddBook/index.js
@@ -2,23 +2,45 @@ import React, { useState } from 'react';
 import './styles.css';
 import API_BASE_URL from '../../config';
 
-
 function AddBook() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [book, setBook] = useState({
+    title: "",
+    author: "",
+    isbn_number: "",
+    summary: "",
+    cover_image: null
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBook(prevState => ({
+        ...prevState,
+        [name]: value
+    }));
+  };
+
+  const handleImageChange = (e) => {
+    setBook(prevState => ({
+        ...prevState,
+        cover_image: e.target.files[0]
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    let formData = new FormData();
+    formData.append('title', book.title);
+    formData.append('author', book.author);
+    formData.append('isbn_number', book.isbn_number);
+    formData.append('summary', book.summary);
+    if (book.cover_image) {
+        formData.append('cover_image', book.cover_image);
+    }
+
     fetch(`${API_BASE_URL}books/`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: title,
-        author: author
-      }),
+      body: formData,
     })
     .then(response => response.json())
     .then(data => {
@@ -33,11 +55,23 @@ function AddBook() {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Title: </label>
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+          <input type="text" name="title" value={book.title} onChange={handleInputChange} required />
         </div>
         <div>
           <label>Author: </label>
-          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} required />
+          <input type="text" name="author" value={book.author} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>ISBN Number: </label>
+          <input type="text" name="isbn_number" value={book.isbn_number} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Summary: </label>
+          <textarea name="summary" value={book.summary} onChange={handleInputChange}></textarea>
+        </div>
+        <div>
+          <label>Cover Image: </label>
+          <input type="file" onChange={handleImageChange} />
         </div>
         <button type="submit">Add</button>
       </form>
